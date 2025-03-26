7b961eb93486a2b55516fbc1feaeacfb6b826d72
Griffin Kuchar <griffin.kuchar@gmail.com>
2025-03-25 21:52:20 -0500

    Initialize Spring Boot Backend Project

---
+++ backend/frogcrew-event-scheduler/src/main/java/edu/tcu/cs/frogcreweventscheduler/FrogcrewEventSchedulerApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcreweventscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogcrewEventSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogcrewEventSchedulerApplication.class, args);
+    }
+
+}
---
+++ backend/frogcrew-event-scheduler/src/test/java/edu/tcu/cs/frogcreweventscheduler/FrogcrewEventSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.frogcreweventscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogcrewEventSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
